c1df523a5527c70d69ed147e391da8747e10b6b9
tak <re795h@gmail.com>
2023-02-26 18:48:36 +0900

    aa

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -3,6 +3,6 @@
 CHAIN_HOME=$(pwd)/.chaindata
 CHAIN_ID=alphachain_9000-1
 
-alphachaind tx gov submit-legacy-proposal software-upgrade cosmovisor-test --title=cosmovisor-test-1 --description='cosmovisor test upgrade' --upgrade-height=50 --upgrade-info='{"binaries":{"darwin/arm64":"https://github.com/tak1827/alpha-chain/releases/download/v0.0.2/alphachaind?checksum=sha256:2dd43260fa8028d6553caa266fc7a75f6913b02e41e4845eac42767106e79405"}}' --deposit=10000000aphoton --gas=300000 --from=valkey --keyring-backend=test --yes --home=$CHAIN_HOME --chain-id=$CHAIN_ID --trace --sequence=1
+alphachaind tx gov submit-legacy-proposal software-upgrade cosmovisor-test --title=cosmovisor-test-1 --description='cosmovisor test upgrade' --upgrade-height=100 --upgrade-info='{"binaries":{"darwin/arm64":"https://github.com/tak1827/alpha-chain/releases/download/v0.0.3/alphachaind?checksum=sha256:6023f93e5def2c076b7ddf0bedb7d0e6d466005cfcd27e1d4b38d671f9f68dc8"}}' --deposit=10000000aphoton --gas=300000 --from=valkey --keyring-backend=test --yes --home=$CHAIN_HOME --chain-id=$CHAIN_ID --trace --sequence=1
 
 alphachaind tx gov vote 1 yes --from=valkey --keyring-backend=test --yes --home=$CHAIN_HOME --chain-id=$CHAIN_ID --sequence=2
